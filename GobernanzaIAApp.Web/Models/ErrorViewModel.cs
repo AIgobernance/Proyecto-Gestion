aa428ffffa94f10da4f869b9ed59ab552770bcef
AIgobernance <pgestionti@gmail.com>
2025-11-03 18:57:18 -0500

    Delete GobernanzaIAApp.Web directory

--- GobernanzaIAApp.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace GobernanzaIAApp.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
